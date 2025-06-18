fa9921fda5f5114577bec3126301cb28e806edb1
Roman784 <rmironenko50@gmail.com>
2025-06-18 17:15:33 +0300

    Debug keys

---
+++ Assets/_Project/Develop/Utils/DebugKeys.cs
@@ -0,0 +1,25 @@
+using GameRoot;
+using UnityEngine;
+
+namespace Utils
+{
+    public class DebugKeys : MonoBehaviour
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        public static void Create()
+        {
+            var go = new GameObject("[DEBUG_KEYS]");
+            go.AddComponent<DebugKeys>();
+            DontDestroyOnLoad(go);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                if (Input.GetKeyUp(KeyCode.R))
+                    GameAutostarter.AutostartGame();
+            }
+        }
+    }
+}
