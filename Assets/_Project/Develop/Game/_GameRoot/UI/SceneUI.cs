e9467dda15583c8b12b4917ebbcc281dd89d8e63
Roman784 <rmironenko50@gmail.com>
2025-06-18 11:26:41 +0300

    Displaying the progress of passing levels

--- Assets/_Project/Develop/Game/_GameRoot/UI/SceneUI.cs
+++ Assets/_Project/Develop/Game/_GameRoot/UI/SceneUI.cs
@@ -1,6 +1,5 @@
 using Configs;
-using System.Collections.Generic;
-using Theme;
+using GameState;
 using UnityEngine;
 using Zenject;
 
@@ -9,15 +8,19 @@ namespace UI
     public class SceneUI : MonoBehaviour
     {
         protected IConfigsProvider _configsProvider;
+        protected IGameStateProvider _gameStateProvider;
         protected PopUpsProvider _popUpsProvider;
 
         protected GameConfigs GameConfigs => _configsProvider.GameConfigs;
         protected UIConfigs UIConfigs => GameConfigs.UIConfigs;
+        protected GameState.GameState GameState => _gameStateProvider.GameStateProxy.State;
 
         [Inject]
-        private void Construct(IConfigsProvider configsProvider, PopUpsProvider popUpsProvider)
+        private void Construct(IConfigsProvider configsProvider, IGameStateProvider gameStateProvider,
+                               PopUpsProvider popUpsProvider)
         {
             _configsProvider = configsProvider;
+            _gameStateProvider = gameStateProvider;
             _popUpsProvider = popUpsProvider;
         }
 
--- Assets/_Project/Develop/Game/_LevelMenu/UI/LevelMenuUI.cs
+++ Assets/_Project/Develop/Game/_LevelMenu/UI/LevelMenuUI.cs
@@ -9,10 +9,20 @@ namespace UI
 
         public void CreateButtons()
         {
+            var lastCompletedLevelNumber = GameState.LastCompletedLevelNumber;
+
             for (int i = 0; i < 20; i++)
             {
+                var number = i + 1;
+
+                var progress = LevelButtonProgress.Uncompleted; 
+                if (number <= lastCompletedLevelNumber)
+                    progress = LevelButtonProgress.Completed;
+                else if (number == lastCompletedLevelNumber + 1)
+                    progress = LevelButtonProgress.Current;
+
                 var newButton = Instantiate(_levelButtonPrefab);
-                newButton.Init((LevelButtonMode)Random.Range(0, 3), (LevelButtonProgress)Random.Range(0, 3));
+                newButton.Init((LevelButtonMode)Random.Range(0, 3), progress);
 
                 _levelButtonsLayout.LayOut(newButton.GetComponent<RectTransform>(), i);
             }
