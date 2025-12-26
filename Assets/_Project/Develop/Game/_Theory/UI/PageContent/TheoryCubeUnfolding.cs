6a69c7d2b021acc9d91a3f0006c7a7f9e6b88d14
Роман Мироненко <79697765+Roman784@users.noreply.github.com>
2025-12-26 18:57:11 +0300

    Fix: cube unfolding layout

--- Assets/_Project/Develop/Game/_Theory/UI/PageContent/TheoryCubeUnfolding.cs
+++ Assets/_Project/Develop/Game/_Theory/UI/PageContent/TheoryCubeUnfolding.cs
@@ -10,6 +10,7 @@ namespace Theory
     {
         [SerializeField] private CubeConfigs _cubeConfigs;
         [SerializeField] private List<TheoryCubeCell> _cells;
+        [SerializeField] private RectTransform _container;
 
         [Space]
 
@@ -17,9 +18,11 @@ namespace Theory
 
         private void Awake()
         {
+            var wordsCount = _cubeConfigs.Words.Count;
+
             for (int i = 0; i < _cells.Count; i++)
             {
-                _cells[i].gameObject.SetActive(i < _cubeConfigs.Words.Count);
+                _cells[i].gameObject.SetActive(i < wordsCount);
 
                 if (i >= _cubeConfigs.Words.Count) continue;
 
@@ -31,6 +34,34 @@ namespace Theory
 
             var cubeName = $" Û· {_cubeConfigs.Number}. {_cubeConfigs.Name}";
             _cubeNameView.text = cubeName;
+
+            var cellSize = 172;
+            if (wordsCount < 5)
+            {
+                _container.sizeDelta = new Vector2(_container.sizeDelta.x, cellSize);
+
+                if (wordsCount % 2 == 1)
+                {
+                    foreach (var cell in _cells)
+                    {
+                        var rect = cell.GetComponent<RectTransform>();
+                        rect.anchoredPosition += new Vector2(cellSize / 2, 0);
+                    }
+                }
+            }
+            else if (wordsCount < 6)
+            {
+                _container.sizeDelta = new Vector2(_container.sizeDelta.x, cellSize * 2);
+                foreach (var cell in _cells)
+                {
+                    var rect = cell.GetComponent<RectTransform>();
+                    rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, rect.anchoredPosition.y - cellSize / 2);
+                }
+            }
+            else
+            {
+                _container.sizeDelta = new Vector2(_container.sizeDelta.x, cellSize * 3);
+            }
         }
     }
 }
